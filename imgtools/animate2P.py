decac16e2fc95e73e773bc327a47628b6ae432dc
Dylan Martins <49732896+dylanmmartins@users.noreply.github.com>
2025-05-01 13:38:18 -0700

    Update animate2P.py

--- imgtools/animate2P.py
+++ imgtools/animate2P.py
@@ -7,9 +7,9 @@ import imgtools
 def animate_stack_from_2P(imgpath=None, savename=None):
 
     if imgpath is None:
-        imgpath = r'T:\axonal_imaging_LP\250426_DMM_DMM046_LPaxons\fm5\file_00005.tif'
+        imgpath = r'C:\Data\dylan\LPaxons\recordings_for_demo\250426_DMM_DMM046_LPaxons_f05.tif'
     if savename is None:
-        savename = 'f05_demo_vid_60Hz_win5_v01.mp4'
+        savename = '250426_freely_moving_f05_demo_vid_60Hz_win8_v01.mp4'
 
     savedir_ = os.path.split(os.path.split(imgpath)[0])[0]
     savepath = os.path.join(savedir_, savename)
@@ -17,11 +17,11 @@ def animate_stack_from_2P(imgpath=None, savename=None):
     # Animate a full tiff stack of two-photon data.
     img_rollavg = imgtools.rolling_average(imgtools.load_tif_stack(
         imgpath, doReg=True, doNorm=False
-    ))
+    ), window=8)
     plot_stack = []
     for f in tqdm(range(np.size(img_rollavg,0))):
         fig = plt.figure(dpi=300, figsize=(4,4))
-        plt.imshow(img_rollavg[f,:,:], cmap='gray', vmin=0, vmax=300)
+        plt.imshow(img_rollavg[f,:,:], cmap='gray', vmin=0, vmax=450) # 2000 for benchtop, 300 for mini2p
         plt.axis('off')
         plt.tight_layout()
         plot_stack.append(imgtools.fmt_figure(fig))
@@ -29,5 +29,8 @@ def animate_stack_from_2P(imgpath=None, savename=None):
     imgtools.write_animation(
         plot_stack,
         savepath,
-        60
+        60 # 
     )
+
+if __name__ == '__main__':
+    animate_stack_from_2P()
