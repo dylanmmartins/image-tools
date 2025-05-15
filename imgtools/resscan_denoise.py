8d78aaa02f3c9fb2e13ab5631a9bd2bb54cdc7bd
Dylan Martins <49732896+dylanmmartins@users.noreply.github.com>
2025-05-14 17:22:16 -0700

    Update resscan_denoise.py

--- imgtools/resscan_denoise.py
+++ imgtools/resscan_denoise.py
@@ -17,28 +17,33 @@ def resscan_denoise(tif_path=None, ret=False):
     # If ret is true, the function will return the image stack with a short (3 frame)
     # rolling average applied.
 
+    print('Select tif stack.')
     if tif_path is None:
         tif_path = imgtools.select_file(
             'Select tif stack.',
             filetypes=[('TIF', '*.tif'),('TIF','*.tiff'),]
         )
 
+    print('Denoising {}'.format(tif_path))
+
     rawimg = imgtools.load_tif_stack(tif_path)
 
     base_path = os.path.split(tif_path)[0]
+    tif_name = os.path.split(tif_path)[1]
     pdf = PdfPages(os.path.join(base_path, 'denoising_figs.pdf'))
 
     mean_of_banded_block = np.mean(rawimg[:,412:,:],1)
 
-    fig = plt.figure(figsize=(7,4), dpi=300)
-    plt.imshow(mean_of_banded_block, aspect=0.08, cmap='gray', vmin=0, vmax=100)
+    fig = plt.figure(figsize=(6,6), dpi=300)
+    plt.imshow(mean_of_banded_block, aspect=0.05, cmap='gray')
     plt.colorbar()
     plt.xlabel('y pixels')
     plt.ylabel('time (frames)')
     plt.tight_layout()
     pdf.savefig(fig)
+    plt.close()
 
-
+    print('Calculating noise pattern.')
     f_size = np.shape(rawimg[0,:,:])
     noise_pattern = np.zeros_like(rawimg)
     for f in tqdm(range(np.size(noise_pattern,0))):
@@ -50,14 +55,20 @@ def resscan_denoise(tif_path=None, ret=False):
     f = 500
     fig, [ax1,ax2,ax3] = plt.subplots(1,3, figsize=(5.5,3), dpi=300)
     ax1.imshow(rawimg[f,:,:], cmap='gray', vmin=0, vmax=200)
-    ax2.imshow(noise_pattern[f,:,:], cmap='gray')
+    ax2.imshow(noise_pattern[f,:,:], cmap='gray',
+               vmin=np.min(noise_pattern[f,:,:]),
+               vmax=np.max(noise_pattern[f,:,:]))
     ax3.imshow(newimg[f,:,:], cmap='gray', vmin=0, vmax=200)
     ax1.axis('off')
     ax2.axis('off')
     ax3.axis('off')
     fig.tight_layout()
     pdf.savefig(fig)
+    plt.close()
 
+    del rawimg
+
+    print('Calculting diagnotic statistics across recording.')
     # mean across frames for output image data (is there drift over time or large jumps? it should not.)
     meanF = np.mean(newimg,axis=(1,2))
     # mean across frames for the oinse pattern (does it have large jumps or drift over time? it should)
@@ -76,18 +87,60 @@ def resscan_denoise(tif_path=None, ret=False):
     ax2.set_title('putative noise pattern')
     fig.tight_layout()
     pdf.savefig(fig)
+    plt.close()
+
+    noise_len = np.size(noise_pattern,0)
+
+    del noise_pattern
 
     # Save two versions of the output video: one raw video, one with a small
     # rolling average, and one with a large rolling average.
     # For the small rolling average, apply a 400 msec smoothing window
+    print('Calculating rolling average (short window).')
     sra_newimg = imgtools.rolling_average(newimg, 3)
-    # for the large rolling average, apply a 1600 msec smoothing window (this
-    # is probably only useful for visualization)
-    lra_newimg = imgtools.rolling_average(newimg, 12)
 
     full_numF = np.size(newimg,0)
     sra_len = np.size(sra_newimg,0)
+
+    # Make sure corrected values are in the bounds of the data type that
+    # will be used when the tif is written.
+    sra_newimg[sra_newimg<np.iinfo(np.uint16).min] = np.iinfo(np.uint16).min
+    sra_newimg[sra_newimg>np.iinfo(np.uint16).max] = np.iinfo(np.uint16).max
+
+    tif_name_noext = os.path.splitext(tif_name)[0]
+    s_savefilename = os.path.join(base_path, '{}_denoised_SRA.tif'.format(tif_name_noext))
+    print('Writing {}'.format(s_savefilename))
+    with tifffile.TiffWriter(s_savefilename, bigtiff=True) as savestack:
+        savestack.write(
+            data=sra_newimg.astype(np.uint16),
+            dtype=np.uint16,
+            shape=sra_newimg.shape,
+            photometric='MINISBLACK'
+        )
+    del sra_newimg
+
+    # For the large rolling average, apply a 1600 msec smoothing window (this
+    # is probably only useful for visualization)
+    print('Calculating rolling average (long window).')
+    lra_newimg = imgtools.rolling_average(newimg, 12)
     lra_len = np.size(lra_newimg,0)
+
+    lra_newimg[lra_newimg<np.iinfo(np.uint16).min] = np.iinfo(np.uint16).min
+    lra_newimg[lra_newimg>np.iinfo(np.uint16).max] = np.iinfo(np.uint16).max
+
+    l_savefilename = os.path.join(base_path, '{}_denoised_LRA.tif'.format(tif_name_noext))
+    with tifffile.TiffWriter(l_savefilename, bigtiff=True) as savestack:
+        savestack.write(
+            data=lra_newimg.astype(np.uint16),
+            dtype=np.uint16,
+            shape=lra_newimg.shape,
+            photometric='MINISBLACK'
+        )
+
+    del newimg
+    del lra_newimg
+
+    pdf.close()
 
     frame_note = (
         'The full tif stack had {} frames. The denoised tif stack with a short running average '
@@ -95,33 +148,12 @@ def resscan_denoise(tif_path=None, ret=False):
         'the denoised stacks to other data streams, subtract diff/2 from the start and end. '
         'Adjust SRA by {} and LRA by {}.'
     )
-    sra_adjust = int((np.size(noise_pattern,0)-np.size(sra_len,0))/2)
-    lra_adjust = int((np.size(noise_pattern,0)-np.size(lra_len,0))/2)
+    sra_adjust = int((noise_len-np.size(sra_len,0))/2)
+    lra_adjust = int((noise_len-np.size(lra_len,0))/2)
     frame_note = frame_note.format(full_numF, sra_len, lra_len, sra_adjust, lra_adjust)
     with open('note_on_denoised_tif_dims.txt', 'w') as file:
         file.write(frame_note)
     print(frame_note)
-
-    base_tif_path = os.path.splitext(tif_path)[0]
-    s_savefilename = base_tif_path+'_denoised_SRA.tif'
-    with tifffile.TiffWriter(s_savefilename, bigtiff=True) as savestack:
-        savestack.write(
-            data=sra_newimg,
-            dtype=np.uint16,
-            shape=sra_newimg.shape,
-            photometric='MINISBLACK'
-        )
-
-    l_savefilename = base_tif_path+'_denoised_SRA.tif'
-    with tifffile.TiffWriter(l_savefilename, bigtiff=True) as savestack:
-        savestack.write(
-            data=lra_newimg,
-            dtype=np.uint16,
-            shape=lra_newimg.shape,
-            photometric='MINISBLACK'
-        )
-
-    pdf.close()
 
     if ret:
         return sra_newimg
